03efdeeb697fdf3c60b7f59d915997b548979e11
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2020-12-11 19:15:29 +0530

    index.js of an example using React.js

---
+++ React.js/Another-example/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+//const currentDate= new Date();
+//const currentTime = currentDate.getTime();
+//const year = cuurentDate.getFullYear();
+
+ReactDOM.render(
+  <App />,
+
+  document.getElementById("root")
+);
