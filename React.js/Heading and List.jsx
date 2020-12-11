e70b91d4c60c3905c37bf38003fca4522bce1fab
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2020-12-11 19:13:26 +0530

    heading and List.jsx files of React.js

---
+++ React.js/Heading and List.jsx
@@ -0,0 +1,31 @@
+/////Heading/////
+
+import React from "react";
+//import ReactDOM from "react-dom";
+
+function Heading() {
+  return <h1>My favuorite Foods</h1>;
+}
+
+export default Heading;
+
+////List////
+
+import React from "react";
+//import ReactDOM from "react-dom";
+
+
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>vada</li>
+        <li>Dosa</li>
+        <li>puri</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
+
