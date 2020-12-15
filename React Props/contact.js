8b161ed7177bb7af1468c2cb9f95e60203a3e437
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2020-12-15 19:53:05 +0530

    Updated contact list of an example.

--- React Props/contact.js
+++ React Props/contact.js
@@ -1,21 +1,27 @@
 const contacts = [
   {
-   name: "Beyonce",
-    imgURL: "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    id: 1,
+    name: "Darshan",
+    imgURL:
+      "https://1847884116.rsc.cdn77.org/kannada/gallery/actor/darshan/dharsan_kannada_actor_13.jpg",
     phone: "+123 456 789",
-    email: "b@beyonce.com"
+    email: "dboss@gmail.com"
   },
   {
-    name: "Jack Bauer",
-    imgURL: "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    id: 2, 
+    name: "Kiccha Sudeep",
+    imgURL:
+      "https://static.toiimg.com/thumb/msid-68724085,imgsize-136653,width-800,height-600,resizemode-75/68724085.jpg",
     phone: "+987 654 321",
-    email: "jack@nowhere.com"
+    email: "sudeepo@kannada.com"
   },
-  {
-    name: "Chuck Norris",
-    imgURL: "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+  { 
+    id: 3,
+    name: "Soundarya",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/1300804848800002048/Mb5ASngU_400x400.jpg",
     phone: "+918 372 574",
-    email: "gmail@chucknorris.com"
+    email: "Soundarya@gamil.com"
   }
 ];
 
