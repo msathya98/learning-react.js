f1f4883500f1651a7f48b12cdda1ea2889677eb1
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2020-12-18 19:26:16 +0530

    index.js of spread operator concept in React.js

---
+++ Spread operators/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+/*const citrus = ["lime", "lemon", "orange"];
+const fruit = ["apple",...citrus, "banana", "kiwi" ];
+
+console.log(fruit);*/
+
